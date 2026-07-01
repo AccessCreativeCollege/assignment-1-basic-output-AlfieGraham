e8b7656976c5dac6fa2cc60ebf8b8ae974d87491
Alfie Graham <63206909+alfgram@users.noreply.github.com>
2026-07-01 12:43:45 +0100

    done

--- task.py
+++ task.py
@@ -4,22 +4,24 @@
 
 # Task 1: Print your first name
 # TODO: print your first name only
+print("Alfie")
 
 
 # Task 2: Print the message: Hello, World!
 # TODO: print exactly: Hello, World!
-
+print("Hello, World!")
 
 # Task 3: Print the number 42
 # TODO: print the number 42
-
+print(42)
 
 # Task 4: Print the result of adding 10 and 5
 # TODO: print the result of 10 + 5
-
+print(10 + 5)
 
 # Task 5: Print two lines using a single print statement
 # Line one should read: Line one
 # Line two should read: Line two
 # Hint: use \n inside your string
 # TODO: print both lines using one print() call
+print("Line one\nLine two")
